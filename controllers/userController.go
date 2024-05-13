d5c8e056afd05a07de6396d86445c437623d88fb
muhammadderic <muhammad.donny.e@gmail.com>
2024-05-13 19:48:59 +0700

    Upgrade user registration logic, install bcrypt, and create user model

--- controllers/userController.go
+++ controllers/userController.go
@@ -4,11 +4,70 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/bcrypt"
+
+	"photomanagerapp/database"
+	"photomanagerapp/models"
 )
 
 func UserRegister(c *gin.Context) {
+	// Define request struct to capture user registration data
+	var request struct {
+		Username string
+		Email    string
+		Password string
+	}
+
+	// Bind incoming request data to the struct
+	if c.Bind(&request) != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Failed request!",
+		})
+		return
+	}
+
+	// Validate required fields are not empty
+	if request.Username == "" || request.Email == "" || request.Password == "" {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Username, email, or password cannot be empty!",
+		})
+		return
+	}
+
+	// Validate password length
+	if len(request.Password) < 6 {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Password's length minimum 6 characters!",
+		})
+		return
+	}
+
+	// Hash the password using bcrypt for secure storage
+	encrypt, err := bcrypt.GenerateFromPassword([]byte(request.Password), 10)
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Failed to encrypt the password!",
+		})
+		return
+	}
+
+	// Create a new user object with validated and hashed data
+	newUser := models.User{Username: request.Username, Email: request.Email, Password: string(encrypt)}
+
+	// Insert the new user data into the database
+	res := database.DB.Create(&newUser)
+
+	if res.Error != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Registration Failed!",
+		})
+		return
+	}
+
+	// Respond with success message on successful registration
 	c.JSON(http.StatusOK, gin.H{
-		"message": "Register a user",
+		"message": "Registration Successfull!",
 	})
 }
 
---
+++ models/userModel.go
@@ -0,0 +1,10 @@
+package models
+
+type User struct {
+	ID         int    `gorm:"primaryKey" json:"id"`
+	Username   string `gorm:"type:varchar(255);not null" json:"username"`
+	Email      string `gorm:"type:varchar(255);not null;unique" json:"email"`
+	Password   string `gorm:"type:varchar(255);not null" json:"password"`
+	Created_At string `gorm:"type:timestamp;default:CURRENT_TIMESTAMP()" json:"created_at"`
+	Updated_At string `gorm:"type:timestamp;default:CURRENT_TIMESTAMP()" json:"updated_at"`
+}
